ba99e57c647f3d20e7b159c1488f7ec8eaee115e
papunatsiskadze <papunaciskadze@gmail.com>
2022-05-26 17:02:27 +0400

    commit

---
+++ js.js
@@ -0,0 +1,31 @@
+// var people=["levani","shota","gurami","zurabi"];
+
+// for (var i=people.length -1 ; i>=0; i--){
+//     console.log(people[i]);
+// }
+
+
+
+
+
+// var korpusebi=[59,60,61,62,63,64];
+// // for (var i=0; i<korpusebi.length; i++){
+// //     document.write(korpusebi[i] + "</br>");
+// // }
+// for (var i=korpusebi.length -1; i>=0; i--){
+//     document.write(korpusebi[i] + "</br>" )
+// }
+// for (var index in korpusebi){
+//     document.write(korpusebi[index]);
+// }
+// var index=0;
+// while (index < korpusebi.length){
+//     console.log(korpusebi[index]);
+//     index++;
+// }
+var x=1;
+do {
+    document.write(x * x + "</br>");
+    x++
+}
+while (x<=10);
